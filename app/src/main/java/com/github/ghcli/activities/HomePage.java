14347a5df27737e201f48af9675118d534b3d8ac
liraop <lirapdo@gmail.com>
2017-11-13 23:18:41 -0300

    add select fragment method

--- app/src/main/java/com/github/ghcli/activities/HomePage.java
+++ app/src/main/java/com/github/ghcli/activities/HomePage.java
@@ -17,8 +17,8 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        mBottomNav = (BottomNavigationView) findViewById(R.id.navigation);
-        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        navBar = (BottomNavigationView) findViewById(R.id.navigation);
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 selectFragment(item);
@@ -30,10 +30,45 @@ public class HomePage extends AppCompatActivity {
 
         if (savedInstanceState != null) {
             mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
-            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
+            selectedItem = navBar.getMenu().findItem(mSelectedItem);
         } else {
-            selectedItem = mBottomNav.getMenu().getItem(0);
+            selectedItem = navBar.getMenu().getItem(0);
         }
         selectFragment(selectedItem);
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                frag = MenuFragment.newInstance(getString(R.string.text_home),
+                        getColorFromRes(R.color.color_home));
+                break;
+            case R.id.menu_notifications:
+                frag = MenuFragment.newInstance(getString(R.string.text_notifications),
+                        getColorFromRes(R.color.color_notifications));
+                break;
+            case R.id.menu_search:
+                frag = MenuFragment.newInstance(getString(R.string.text_search),
+                        getColorFromRes(R.color.color_search));
+                break;
+        }
+
+        // update selected item
+        mSelectedItem = item.getItemId();
+
+        // uncheck the other items.
+        for (int i = 0; i< navBar.getMenu().size(); i++) {
+            MenuItem menuItem = navBar.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
+
+
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, frag, frag.getTag());
+            ft.commit();
+        }
     }
 }
