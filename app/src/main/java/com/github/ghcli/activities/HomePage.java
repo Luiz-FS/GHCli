931c58cb79703311636633ffec88284df15151aa
liraop <lirapdo@gmail.com>
2017-11-13 23:15:56 -0300

    add navbar button action

--- app/src/main/java/com/github/ghcli/activities/HomePage.java
+++ app/src/main/java/com/github/ghcli/activities/HomePage.java
@@ -2,17 +2,38 @@ package com.github.ghcli.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.design.widget.BottomNavigationView;
+
 
 import com.github.ghcli.R;
 
 public class HomePage extends AppCompatActivity {
 
+    private BottomNavigationView navBar;
+    private int mSelectedItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        mBottomNav = (BottomNavigationView) findViewById(R.id.navigation);
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return true;
+            }
+        });
+
+        MenuItem selectedItem;
+
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = mBottomNav.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
     }
 }
