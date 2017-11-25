d8546db0cd39a06996fcaf7704a28dabbc4d96e0
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-11-24 22:18:48 -0300

    Add getFollowers method an set strict mode police

--- app/src/main/java/com/github/ghcli/activities/HomePage.java
+++ app/src/main/java/com/github/ghcli/activities/HomePage.java
@@ -2,6 +2,7 @@ package com.github.ghcli.activities;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.BottomNavigationView;
@@ -14,7 +15,15 @@ import android.util.Log;
 
 
 import com.github.ghcli.R;
+import com.github.ghcli.models.GitHubUser;
+import com.github.ghcli.service.ServiceGenerator;
+import com.github.ghcli.service.clients.IGitHubUser;
 import com.github.ghcli.util.Authentication;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
 
 public class HomePage extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener, FollowersFragment.OnFragmentInteractionListener, ReposFragment.OnFragmentInteractionListener {
     private static final String SELECTED_ITEM = "arg_selected_item";
@@ -26,6 +35,8 @@ public class HomePage extends AppCompatActivity implements ProfileFragment.OnFra
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
+        StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_home_page);
         Toolbar toolbar = findViewById(R.id.toolbar);
         navBar = (BottomNavigationView) findViewById(R.id.navigation);
--- app/src/main/java/com/github/ghcli/service/clients/IGitHubUser.java
+++ app/src/main/java/com/github/ghcli/service/clients/IGitHubUser.java
@@ -2,6 +2,8 @@ package com.github.ghcli.service.clients;
 
 import com.github.ghcli.models.GitHubUser;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
@@ -13,4 +15,7 @@ public interface IGitHubUser {
 
     @GET("user")
     Call<GitHubUser> getUser(@Header("Authorization") String credentials);
+
+    @GET("user/followers")
+    Call<List<GitHubUser>> getFollowers(@Header("Authorization") String credentials);
 }
